27e80eefbb5e4cc7c1b9fa297366def9eaf06050
Jeremy <jeremygrondahl@gmail.com>
2015-09-07 14:07:15 -0500

    Added tootsie pop image. Working on using the image as a weapon in the game.

--- js/main.js
+++ js/main.js
@@ -2,25 +2,136 @@
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+
+var Bullet = function (game, key) {
+
+  Phaser.Sprite.call(this, game, 0, 0, key);
+
+  this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+
+  this.anchor.set(0.5);
+
+  this.checkWorldBounds = true;
+  this.outOfBoundsKill = true;
+  this.exists = false;
+
+  this.tracking = false;
+  this.scaleSpeed = 0;
+
+};
+
+Bullet.prototype = Object.create(Phaser.Sprite.prototype);
+Bullet.prototype.constructor = Bullet;
+
+Bullet.prototype.fire = function (x, y, angle, speed, gx, gy) {
+
+  gx = gx || 0;
+  gy = gy || 0;
+
+  this.reset(x, y);
+  this.scale.set(1);
+
+  this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
+
+  this.angle = angle;
+
+  this.body.gravity.set(gx, gy);
+
+};
+
+Bullet.prototype.update = function () {
+
+  if (this.tracking)
+  {
+      this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
+  }
+
+  if (this.scaleSpeed > 0)
+  {
+      this.scale.x += this.scaleSpeed;
+      this.scale.y += this.scaleSpeed;
+  }
+
+};
+
+
+var pops = {};
+
+
+////////////////////////////////////////////////////
+    //  A single pop is fired in front of the sun //
+    ////////////////////////////////////////////////////
+
+    pops.SingleBullet = function (game) {
+
+        Phaser.Group.call(this, game, game.world, 'Single Bullet', false, true, Phaser.Physics.ARCADE);
+
+        this.nextFire = 0;
+        this.bulletSpeed = 600;
+        this.fireRate = 100;
+
+        for (var i = 0; i < 64; i++)
+        {
+            this.add(new Bullet(game, 'pop'), true);
+        }
+
+        return this;
+
+    };
+
+    pops.SingleBullet.prototype = Object.create(Phaser.Group.prototype);
+    pops.SingleBullet.prototype.constructor = pops.SingleBullet;
+
+    pops.SingleBullet.prototype.fire = function (source) {
+
+        if (this.game.time.time < this.nextFire) { return; }
+
+        var x = source.x + 10;
+        var y = source.y + 10;
+
+        this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, 0, 0);
+
+        this.nextFire = this.game.time.time + this.fireRate;
+
+    };
 function preload() {
     game.load.image('com', 'assets/computer_monitors.png');
     game.load.spritesheet('sun', 'assets/enemies/hot_sun.png', 32, 32);
     game.load.spritesheet('bug', 'assets/enemies/Bug.png', 32, 32);
+    game.load.spritesheet('pop', 'assets/pop.png', 60, 60);
+    game.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
 
 }
 
+ //  The core game loop
+
+  var PhaserGame = function () {
+
+      this.weapons = [];
+      this.currentWeapon = 0;
+      this.weaponName = null;
+
+  };
+
+
 var enemies;
+var explosions;
+
 
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  pops.push(new pops.SingleBullet(this.game));
+
+    
   
   // game.add.sprite(68, 100, 'com');
   enemies = game.add.group();
   enemies.enableBody = true;
   game.time.events.loop(550, createSprite, this);
 
-  hotSun = game.add.sprite(200, 200, 'sun');
+  hotSun = game.add.sprite(700, 200, 'sun');
   hotSun.scale.set(2);
 
   // for (var i = 0; i < 8; i++) {
@@ -49,7 +160,21 @@ function create() {
   
   // enemies.sortDirection = Phaser.Physics.Arcade.LEFT_RIGHT;
 
-  
+  //  An explosion pool
+    explosions = game.add.group();
+    explosions.createMultiple(30, 'kaboom');
+    explosions.forEach(setupInvader, this);
+
+    this.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
+ 
+}
+
+function setupInvader (invader) {
+
+    invader.anchor.x = 0.5;
+    invader.anchor.y = 0.5;
+    invader.animations.add('kaboom');
+
 }
 
 function createSprite() {
@@ -85,6 +210,9 @@ function update() {
 
   cursors = game.input.keyboard.createCursorKeys();
 
+   //  Run collision
+  game.physics.arcade.overlap(enemies, hotSun, collisionHandler, null, this);
+
   //  Reset the players velocity (movement)
   hotSun.body.velocity.x = 0;
 
@@ -119,6 +247,20 @@ function update() {
 
 }
 
+function collisionHandler (bug, hotSun) {
+
+    //  When hotSun hits a bug we kill them both
+    bug.kill();
+    hotSun.kill();
+
+
+    //  And create an explosion :)
+    var explosion = explosions.getFirstExists(false);
+    explosion.reset(bug.body.x, bug.body.y);
+    explosion.play('kaboom', 30, false, true);
+
+}
+
 function checkSprite(sprite) {
 
     try {
@@ -134,6 +276,9 @@ function checkSprite(sprite) {
     }
 
 }
+
+
+
 
 
 
